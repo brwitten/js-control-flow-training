f7978f686bffa257dfd3dec193c278500624f0ee
brwitten <bethrwitten@gmail.com>
2017-09-27 19:48:39 -0700

    Login is completed

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,19 @@
 console.log("login.js loaded");
+
+var userLogin = { userName:'Brwitten', savedPassword:'password'};
+
+var inputPassword = prompt("Greetings, " + userLogin.userName + ". Please enter your password.");
+
+if (inputPassword == userLogin.savedPassword) {
+  alert("Welcome back!")
+} else {
+  var inputPassword2 = prompt("Hmm... that's not right. Try again.")
+} if (inputPassword2 == userLogin.savedPassword) {
+  alert("Welcome back!")
+} else {
+  var inputPassword3 = prompt("Hmm... that's not right. Try one more time.")
+}; if (inputPassword3 == userLogin.savedPassword) {
+  alert("Welcome back!")
+} else {
+  alert("Sorry, it doesn't look like this is going to work out.")
+};
--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,15 +1,10 @@
-console.log("security_questions.js loaded");
-
 var securityQuestions = [
   {question: "What is your favorite sport?", expectedAnswer: "Field hockey"},
   {question: "What is your favorite color?", expectedAnswer: "green"},
   {question: "Cats or dogs?", expectedAnswer: "both!"}
 ];
 
-console.log(securityQuestions[1].question);
-
 var arrayLength = securityQuestions.length;
-console.log(arrayLength);
 
 for (var i = 0; i < arrayLength; i++) {
     var userAnswer = prompt(securityQuestions[i].question);
