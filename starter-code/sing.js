a18bc49677259621f30d6ca6b0c82da08177d552
brwitten <bethrwitten@gmail.com>
2017-09-27 20:11:25 -0700

    Completed sing file

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,13 @@
 console.log("sing.js loaded");
+
+var bottles = prompt("How many verses shall we sing?!")
+
+for (var i = bottles; i >= 0; i--) {
+  if (i > 1) {
+  console.log(i + " bottles of beer on the wall,\n" + i + " bottles of beer!\nTake one down pass it around,\n" + (i-1) + " bottles of beer on the wall!");
+} if (i == 1) {
+  console.log(i + " bottle of beer on the wall,\n" + i + " bottle of beer!\nTake one down pass it around,\n" + "no more bottles of beer on the wall!");
+} if (i == 0) {
+  console.log("No bottles of beer on the wall,\n" + "no more bottles of beer!");
+}
+}
