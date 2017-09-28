625c3e20c672098332a33dbd646c1692b25a5ccc
brwitten <bethrwitten@gmail.com>
2017-09-27 17:31:07 -0700

    working on first section, array is created

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,18 @@
 console.log("security_questions.js loaded");
+
+var securityQuestions = [
+  {question: "What is your favorite sport?", expectedAnswer: "Field hockey"},
+  {question: "What is your favorite color?", expectedAnswer: "green"},
+  {question: "Cats or dogs?", expectedAnswer: "both!"}
+];
+
+console.log("security array is set");
+
+var arrayLength = securityQuestions.length;
+console.log(arrayLength);
+
+for (var i = 0; i <= arrayLength; i++) {
+    //alert(myStringArray[i]);
+    prompt(question);
+    break;
+}
